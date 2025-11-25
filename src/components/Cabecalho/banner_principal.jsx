c2fac77ad8ac7d8b1e63169772281f34126310db
Miqueias Nogueira Santos <miqueiasnogueirasantos567@gmail.com>
2025-11-25 20:22:03 -0300

    Atualizando o banner

--- src/components/Cabecalho/banner_principal.jsx
+++ src/components/Cabecalho/banner_principal.jsx
@@ -3,19 +3,17 @@ import banner from '../image/Banner.png';
 
 function Banner_Principal() {
   return (
+    
     <section id="cabecalho">
-      {/*Fundo e a Imagem */}
       <div className="banner-content-wrapper" style={{ backgroundImage: `url(${banner})` }}>
-        
-        {/* Ícone de Seta Esquerda (Carrossel) */}
-        <button className="banner-slider-arrow left-arrow" aria-label="Slide anterior">
-          &lt;
-        </button>
-        
-        {/* Ícone de Seta Direita (Carrossel) */}
-        <button className="banner-slider-arrow right-arrow" aria-label="Próximo slide">
-          &gt;
-        </button>
+        <div className="hero-limitador"> 
+          <button className="banner-slider-arrow left-arrow" aria-label="Slide anterior">
+            &lt;
+          </button>
+          <button className="banner-slider-arrow right-arrow" aria-label="Próximo slide">
+            &gt;
+          </button>
+       </div>
       </div>
     </section>
   );
